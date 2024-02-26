5bcc5d6e1e335afe32e0889ed438f92a175ffa14
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-02-26 15:58:39 -0400

    Update staff.py
    added some columns to the staff table

--- App/models/staff.py
+++ App/models/staff.py
@@ -1,1 +1,10 @@
+from werkzeug.security import check_password_hash, generate_password_hash
+from App.database import db
 
+class Staff(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    f_name =  db.Column(db.String, nullable=False)
+    l_name =  db.Column(db.String, nullable=False)
+    other_name = db.Column(db.String, nullable=True)
+    staff_type = db.Colunm (db.Integer, nullable = False) #Uses a numerical system for staff type
+                                                          #0 - Lecturer, 1 - TA, 2 - Tutor, 3 - Marker
