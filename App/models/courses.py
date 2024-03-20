8e26de900abd17390cc7509232efeb4b67d066ac
karissa-ss <98049869+karissa-ss@users.noreply.github.com>
2024-03-20 00:50:46 -0400

    Update courses.py

--- App/models/courses.py
+++ App/models/courses.py
@@ -1,7 +1,7 @@
 from App.database import db
 
 class Courses(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    course_id = db.Column(db.String(120), primary_key=True)
     course_name = db.Column(db.String(120), nullable=False, unique=True)
     sem_offered = db.Column(db.Integer)
     type = db.Column(db.Integer) #encodes course type to a numeric value: 1 - Lab, 2 - Tutorial
@@ -11,12 +11,12 @@ class Courses(db.Model):
     numAssessments = = db.Column(db.Integer, nullable = True)
     totalCost = = db.Column(db.Integer, nullable = True)  
 
-class Lecture(Courses):
-    credits = db.Column(db.Float, default = 1)
 
-class Lab(Courses):
-    credits = db.Column(db.Float, default = 0.5)
-
-class Tutorial(Courses):
-    credits = db.Column(db.Float, default = 1)
+    
+class Session(db.Model):
+    course_id = db.Column(
+    type = db.Column(db.Integer) 
+    numCredits = db.Column(db.Float, nullable = False) 
+    numHours = db.Column(db.Float, nullable = False) 
+    numStreams = db.Column(db.Integer) 
     
