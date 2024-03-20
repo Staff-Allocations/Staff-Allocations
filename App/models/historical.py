f089aa32d4018363a0ff1b66152ec5996fdbc1ab
karissa-ss <98049869+karissa-ss@users.noreply.github.com>
2024-03-20 00:51:38 -0400

    Update historical.py

--- App/models/historical.py
+++ App/models/historical.py
@@ -2,7 +2,7 @@ from App.database import db
 
 class Historical(db.Model):
   staffId = db.Column(db.Integer, db.ForeignKey('Staff.id'))
-  courseId= db.Column(db.Integer, db.ForeignKey('Courses.id'))
+  courseId= db.Column(db.String(120), db.ForeignKey('Courses.course_id'))
   yearTaught= db.Column(db.Integer, nullable = True)
   passRate = db.Column(db.Integer, nullable = True)
   
