1fb9b0743e746c39f9fee9f9ca857f56cf04ae81
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-03-27 11:43:07 -0400

    Create courses.py

---
+++ App/views/courses.py
@@ -0,0 +1,5 @@
+from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
+
+from App.controllers import (get_courses)
+
+courses_views = Blueprint('courses_views', __name__, template_folder='../templates')
