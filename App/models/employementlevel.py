ce4a2a104d1a3599c64ae32c4374c7f582fc9722
karissa-ss <98049869+karissa-ss@users.noreply.github.com>
2024-04-06 00:03:51 -0400

    Create employementlevel.py

---
+++ App/models/employementlevel.py
@@ -0,0 +1,8 @@
+from App.database import db
+
+class EmployemnetLevel(db.Model):
+  levelName = db.Column(db.String(120))
+  payRate = db.Column(db.Float) 
+  period = db.Column(db.Integer) 
+  staffID = db.Column(db.Integer, db.ForeignKey('Users.id'))
+  coursesAssigned = db.Column(db.String) 
