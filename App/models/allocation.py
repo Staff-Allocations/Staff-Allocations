665549362159a0d3d75499429a1d27db9b2e0ff6
karissa-ss <98049869+karissa-ss@users.noreply.github.com>
2024-04-06 02:17:13 -0400

    Update allocation.py

--- App/models/allocation.py
+++ App/models/allocation.py
@@ -1,5 +1,10 @@
 from App.database import db
 
 class Allocation(db.Model):
-  staffId = db.Column(db.Integer, db.ForeignKey('Staff.staff_id')
+  staffId = db.Column(db.Integer, db.ForeignKey('Users.id')) 
   courseId = db.Column(db.String(120), db.ForeignKey('Courses.course_id') 
+  streamNum = db.Column(db.Integer) 
+  semester = db.Column(db.Integer) 
+  year = db.Column(db.Integer) 
+
+  def getStaffAssigned(): 
