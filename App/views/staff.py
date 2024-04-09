6117e977b093ed2f67bb2adf8af6cf45427ede1c
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-04-09 11:34:55 -0400

    Update staff.py
    started the staff views

--- App/views/staff.py
+++ App/views/staff.py
@@ -1,5 +1,24 @@
 from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
 
-from App.controllers import (get_staff)
+from App.controllers import (get_staff, create_User)
 
 staff_views = Blueprint('staff_views', __name__, template_folder='../templates')
+
+
+@staff_views.route('/addStaffDetails', methods=['GET','POST'])
+def add_course_page():
+    return render_template('addStaff.html')
+
+@staff_views.route('/addStaff', methods=['GET','POST'])
+def add_course():
+    data = request.form
+    staff_id = data['staffID']
+    email = data['email']      
+    email = email.lower()   #some preprocessing to make output neater later
+    type = data['type']
+    
+    user = create_User(id, email, type)
+    if user:
+        return redirect('/')
+    flash ('Error occured when trying to add staff member')
+    return redirect ('/')
