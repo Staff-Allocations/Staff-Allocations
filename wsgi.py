8db26242c5996abd5c04703078fe98f8ee624f88
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-04-09 10:51:12 -0400

    Update wsgi.py
    added command to test create course

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ from flask.cli import with_appcontext, AppGroup
 
 from App.database import db, get_migrate
 from App.main import create_app
-from App.controllers import ( create_user, get_all_users_json, get_all_users )
+from App.controllers import (create_user, get_all_users_json, get_all_users, create_course, get_all_courses)
 
 # This commands file allow you to create convenient CLI commands for testing controllers
 
@@ -48,6 +48,31 @@ def list_user_command(format):
         print(get_all_users_json())
 
 app.cli.add_command(user_cli) # add the group to the cli
+
+
+course_cli = AppGroup('course', help='course object commands') 
+
+@course_cli.command("create", help="Creates a course")
+@click.argument("course_id")
+@click.argument("course_name")
+@click.argument("sem_offered")
+@click.argument("type")
+def create_course_command(course_id, course_name, sem_offered, type):
+    staffAssigned = 0
+    currStudents =0
+    numAssessments = 1 
+    capacity = 200
+    totalCost = 0
+    create_course(course_id, course_name, sem_offered, type, staffAssigned, currStudents, capacity, numAssessments, totalCost)
+    print(f'{course_id} created!')
+
+@course_cli.command("list", help="Lists courses in the database")
+@click.argument("format", default="string")
+def list_user_command(format):
+    print(get_all_courses())
+
+
+app.cli.add_command(course_cli)
 
 '''
 Test Commands
