8be81cc5085f3fae9e410619a10f723abab98ccc
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-04-09 04:09:41 -0400

    Update __init__.py

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -4,7 +4,7 @@ from .user import user_views
 from .index import index_views
 from .auth import auth_views
 from .courses import course_views
-from .staff imoirt staff_views
+from .staff import staff_views
 
 
 views = [user_views, index_views, auth_views, course_views, staff_views] 
