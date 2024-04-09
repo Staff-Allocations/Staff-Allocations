6e20ea59a04531b8760170f3fc396a3689e3c849
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-04-09 06:14:07 -0400

    Update employmentlevel.py

--- App/models/employmentlevel.py
+++ App/models/employmentlevel.py
@@ -4,5 +4,5 @@ class EmploymnetLevel(db.Model):
   levelName = db.Column(db.String(120))
   payRate = db.Column(db.Float) 
   period = db.Column(db.Integer) 
-  staffID = db.Column(db.Integer, db.ForeignKey('Users.id'), primary_key = True)
+  staffID = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key = True)
   coursesAssigned = db.Column(db.String) 
