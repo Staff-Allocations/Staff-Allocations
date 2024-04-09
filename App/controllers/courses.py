e08ab7a82f06f04e9a612556a01c7e1c3ab79c3c
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-04-09 10:52:11 -0400

    Update courses.py

--- App/controllers/courses.py
+++ App/controllers/courses.py
@@ -4,8 +4,8 @@ from App.config import config
 import requests
 import json
 
-def create_Course(course_id, course_name, sem_offered, type, staffAssigned, currStudents, capacity, numAssessments, totalCost):
-    newCourse = Users (course_id=course_id, course_name=course_name, sem_offered=sem_offered, type=type, staffAssigned=staffAssigned, currStudents=currStudents, capacity=capacity, numAssessments=numAssessments, totalCost=totalCost)
+def create_course(course_id, course_name, sem_offered, type, staffAssigned, currStudents, capacity, numAssessments, totalCost):
+    newCourse = Courses (course_id=course_id, course_name=course_name, sem_offered=sem_offered, type=type, staffAssigned=staffAssigned, currStudents=currStudents, capacity=capacity, numAssessments=numAssessments, totalCost=totalCost)
     db.session.add (newCourse)
     db.session.commit()
     return newCourse
@@ -19,13 +19,13 @@ def get_course(course_id):
 def get_all_courses():
   return Courses.query.all()
 
-def get_Students(course_id):
+def get_students(course_id):
     course = Courses.query.filter_by(course_id = course_id).first
     if (course):
         return course.currStudents
     return None
 
-def get_Staff(course_id):
+def get_staff(course_id):
     course = Courses.query.filter_by(course_id = course_id).first
     if (course):
         return course.staffAssigned
@@ -38,18 +38,14 @@ def add_Staff(course_id, staff_id)
         return course.staffAssigned
     return None
 '''
-def get_NumAssessment(course_id):
+def get_num_assessment(course_id):
     course = Courses.query.filter_by(course_id = course_id).first
     if (course):
         return course.numAssessments
     return None
 
-def get_totalCost(course_id):
+def get_total_cost(course_id):
     course = Courses.query.filter_by(course_id = course_id).first
     if (course):
         return course.totalCost
-    return None
-
-
-
-    
+    return None  
