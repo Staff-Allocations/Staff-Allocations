cfb3344c5f48562e4f9177e9db2fd668c8a64c51
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-04-09 06:02:01 -0400

    Update users.py

--- App/models/users.py
+++ App/models/users.py
@@ -6,52 +6,56 @@ class Users(db.Model):
   email = db.Column(db.String(120), nullable = False, unique = True) 
   type = db.Column(db.Integer) #1- admin , 2- teachingstaff 3- teachingsupport, 4- adminassistant
  
-class TeachingStaff(db.Model): 
+class TeachingStaff(db.Model):
+  id = db.Column(db.Integer, db.ForeignKey('Users.id'), primary_key=True)
   firstName = db.Column(db.String(120))
   lastName =db.Column(db.String(120)) 
   #level = db.Column(db.EmployementLevel) 
   
-  def viewCoursesAssigned(): 
+  # def viewCoursesAssigned(): 
 
-  def recommendMarker(): 
+  # def recommendMarker(): 
 
   
 class TeachingSupport(db.Model):
+  id = db.Column(db.Integer, db.ForeignKey('Users.id'), primary_key=True)
   firstName = db.Column(db.String(120))
   lastName =db.Column(db.String(120)) 
   #level = db.Column(db.EmployementLevel) 
   
-  def viewCoursesAssigned(): 
+  # def viewCoursesAssigned(): 
 
 class Admin(db.Model): 
+  id = db.Column(db.Integer, db.ForeignKey('Users.id'), primary_key=True)
   userID = db.Column(db.Integer, db.ForeignKey('Users.id'))
 
-  def addAllocations():
+  # def addAllocations():
 
-  def reviewAllocations():
+  # def reviewAllocations():
 
-  def viewHistorical(): 
+  # def viewHistorical(): 
     
-  def addStaffProfile(): 
+  # def addStaffProfile(): 
 
-  def updateStaff(): 
+  # def updateStaff(): 
 
-  def confirmStaff():
+  # def confirmStaff():
 
-  def addCourse(): 
+  # def addCourse(): 
     
-  def updateCourses():
+  # def updateCourses():
 
-  def viewBudget():
+  # def viewBudget():
 
-  def projectedBudget(): 
+  # def projectedBudget(): 
 
-class AdminAssistand(db.Model): 
+class AdminAssistant(db.Model):
+  id = db.Column(db.Integer, db.ForeignKey('Users.id'), primary_key=True)
   userID = db.Column(db.Integer, db.ForeignKey('Users.id'))
   
-  def addAllocations():
+  # def addAllocations():
 
-  def reviewAllAllocations(): 
+  # def reviewAllAllocations(): 
 
-  def viewHistorical(): 
+  # def viewHistorical(): 
 
