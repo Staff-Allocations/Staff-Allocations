2a05941f901ae68fa6696ede9665b0a5e5e57f66
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-04-09 10:54:54 -0400

    Update staff.py

--- App/views/staff.py
+++ App/views/staff.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
 
-from App.controllers import (get_Staff)
+from App.controllers import (get_staff)
 
 staff_views = Blueprint('staff_views', __name__, template_folder='../templates')
