c855fa177dede2c06c27e397ff759ddf07ca9d8f
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-04-14 05:07:13 -0400

    Update users.py

--- App/controllers/users.py
+++ App/controllers/users.py
@@ -1,30 +1,32 @@
-from App.models import Users, Courses
+from App.models import Users, Courses, TeachingStaff
 from App.database import db
 from App.config import config
 import requests
 import json
 
-def create_User(id, firstname, lastname, othername, email, level, type, amountPaid):
-    newUser = Users (id=id, firstname=firstname, lastname=lastname, othername=othername, email=email, level=level, type=type, amountPaid=amountPaid)
+def create_ts(id, firstName, lastName, courses, email):
+    newUser = TeachingStaff (id=id, firstName=firstName, lastName=lastName, courses=courses, email=email)
     db.session.add (newUser)
     db.session.commit()
     return newUser
   
-def get_user(id):
-    user = Users.query.filter_by(id = id).first
+def get_ts(id):
+    user = TeachingStaff.query.filter_by(id = id).first
     if (user):
         return user
     return None
 
-def get_all_users():
-  return Users.query.all()
+def get_all_ts():
+  return TeachingStaff.query.all()
 
 def view_courses_assigned(id):
     user = get_user(id)
     if (user):
         return Courses.filter_by(staff_id = id)
     return None
-    
-        
-    
-    
+
+def get_available_staff(course_id):
+    staff = TeachingStaff.query.filter_by(courses=course_id)
+    if (staff): 
+        return staff
+    return None
