895d00d64ebf79fb8554fe704e7bb1b40ad2dd91
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-04-14 05:09:56 -0400

    Update __init__.py

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -5,7 +5,8 @@ from .index import index_views
 from .auth import auth_views
 from .courses import course_views
 from .staff import staff_views
+from .allocations import allocation_views
 
 
-views = [user_views, index_views, auth_views, course_views, staff_views] 
+views = [user_views, index_views, auth_views, course_views, staff_views, allocation_views] 
 # blueprints must be added to this list
