d38f1a5d16612a566fb0fb6410a90a75b5bb4a8e
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-04-14 05:08:15 -0400

    Update courses.py

--- App/models/courses.py
+++ App/models/courses.py
@@ -4,14 +4,14 @@ class Courses(db.Model):
     course_id = db.Column(db.String(120), primary_key=True)
     course_name = db.Column(db.String(120), nullable=False, unique=True)
     sem_offered = db.Column(db.Integer)
-    type = db.Column(db.Integer) #encodes course type to a numeric value: 1 - Lab, 2 - Tutorial
+    type = db.Column(db.String(120)) 
     staffAssigned = db.Column(db.Integer, db.ForeignKey ('users.id')) #Who's currently assigned to the course
     currStudents = db.Column(db.Integer, nullable = True)
     capacity = db.Column(db.Integer, nullable = True)
     numAssessments = db.Column(db.Integer, nullable = False)
     totalCost = db.Column(db.Integer, nullable = True)
 
-    # def getStudens(): 
+    # def getStudents(): 
 
     # def getStaff():
 
