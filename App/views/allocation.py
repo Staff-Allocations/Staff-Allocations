8c2c4933f5357ee9867da1af90690d8215f2fc0f
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-04-14 07:00:09 -0400

    Update and rename allocations.py to allocation.py

---
+++ App/views/allocation.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
+from App.models import db
+from App.controllers import get_allocations, allocate_staff, get_course, get_available_staff, get_all_courses
+
+allocation_views = Blueprint('allocation_views', __name__, template_folder='../templates')
+
+@allocation_views.route('/<course_id>', methods=['GET','POST'])
+def return_staff(course_id):
+    allocations = get_allocations()
+    courses = get_all_courses()
+    staff = get_available_staff(course_id)
+    return render_template('index.html', courseID=course_id, courses = courses, staff = staff,  allocations= allocations )
+
+@allocation_views.route('/<course_id>/<staff_id>', methods = ['GET','POST'])
+def allocateStaff(course_id, staff_id):
+    allocate = allocate_staff(course_id, staff_id)
+    allocations = get_allocations()
+    staff = get_available_staff(course_id)
+    courses = get_all_courses()
+    return url_for ('return_staff', course_id=course_id)
+
+    
