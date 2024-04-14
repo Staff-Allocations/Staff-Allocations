76d4d98e7225e51ca1dbeaf4d3b4fc1b3dbdfa52
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-04-14 06:59:33 -0400

    Update __init__.py

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -5,7 +5,7 @@ from .index import index_views
 from .auth import auth_views
 from .courses import course_views
 from .staff import staff_views
-from .allocations import allocation_views
+from .allocation import allocation_views
 
 
 views = [user_views, index_views, auth_views, course_views, staff_views, allocation_views] 
