b4460816da7fd9db8d86707462a3174081378b62
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-04-14 21:55:03 -0400

    Update allocation.py

--- App/views/allocation.py
+++ App/views/allocation.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
+from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify, url_for
 from App.models import db
-from App.controllers import get_allocations, allocate_staff, get_course, get_available_staff, get_all_courses
+from App.controllers import set_role, get_allocations, allocate_staff, get_course, get_available_staff, get_all_courses
 
 allocation_views = Blueprint('allocation_views', __name__, template_folder='../templates')
 
@@ -13,10 +13,11 @@ def return_staff(course_id):
 
 @allocation_views.route('/<course_id>/<staff_id>', methods = ['GET','POST'])
 def allocateStaff(course_id, staff_id):
-    allocate = allocate_staff(course_id, staff_id)
+    role =set_role(staff_id)
+    allocate = allocate_staff(course_id, staff_id, role)
     allocations = get_allocations()
     staff = get_available_staff(course_id)
     courses = get_all_courses()
-    return url_for ('return_staff', course_id=course_id)
+    return redirect ('/')
 
     
