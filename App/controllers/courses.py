58dcff46ec53b2d1b55b3ba7da5673224784c652
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-04-15 02:48:25 -0400

    Update courses.py
    added update courses

--- App/controllers/courses.py
+++ App/controllers/courses.py
@@ -11,7 +11,7 @@ def create_course(course_id, course_name, sem_offered, type, staffAssigned, curr
     return newCourse
   
 def get_course(course_id):
-    course = Courses.query.filter_by(course_id = course_id).first
+    course = Courses.query.filter_by(course_id = course_id).first()
     if (course):
         return course
     return None
@@ -20,13 +20,13 @@ def get_all_courses():
   return Courses.query.all()
 
 def get_students(course_id):
-    course = Courses.query.filter_by(course_id = course_id).first
+    course = Courses.query.filter_by(course_id = course_id).first()
     if (course):
         return course.currStudents
     return None
 
 def get_staff(course_id):
-    course = Courses.query.filter_by(course_id = course_id).first
+    course = Courses.query.filter_by(course_id = course_id).first()
     if (course):
         return course.staffAssigned
     return None
@@ -39,15 +39,31 @@ def add_Staff(course_id, staff_id)
     return None
 '''
 def get_num_assessment(course_id):
-    course = Courses.query.filter_by(course_id = course_id).first
+    course = Courses.query.filter_by(course_id = course_id).first()
     if (course):
         return course.numAssessments
     return None
 
 def get_total_cost(course_id):
-    course = Courses.query.filter_by(course_id = course_id).first
+    course = Courses.query.filter_by(course_id = course_id).first()
     if (course):
         return course.totalCost
     return None  
 
+def update_course(course_id, course_name, sem_offered, type, staffAssigned, currStudents, capacity, numAssessments, numStreams):
+    course = get_course(course_id)
 
+    if course:
+        course.course_id = course_id
+        course.course_name = course_name
+        course.sem_offered = sem_offered
+        course.type = type
+        course.staffAssigned = staffAssigned
+        course.currStudents = currStudents
+        course.capacity = capacity
+        course.numAssessments = numAssessments
+        course.numStreams = numStreams
+       
+        db.session.commit()
+        return course
+    return None       
