fd91be6973baadb4d2fa2957982e43c450e804cc
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-04-15 07:50:36 -0400

    Update allocation.py

--- App/controllers/allocation.py
+++ App/controllers/allocation.py
@@ -22,3 +22,31 @@ def set_role(staff_id):
     user = TeachingStaff.query.filter_by(id = staff_id).first()
     if (user):
         return user
+
+def delete_allocation(id):
+    allocated= is_allocated(id)
+
+    if allocated:
+        try:
+            db.session.delete(allocated)
+            db.session.commit()
+            return True  #Indicate successful deletion
+        except Exception as e:
+            db.session.rollback() 
+            print(f"Error deleting staff: {e}")
+            return False  #Indicate failure
+    else:
+        return False
+
+
+def get_courses_for_staff(staff_id):
+    allocations = Allocation.query.filter_by(staffId=staff_id).all()
+
+    course_ids = set(allocation.courseId for allocation in allocations)
+    courses = []
+    for course_id in course_ids:
+        course = Courses.query.filter_by(course_id=course_id).first()
+        if course:
+            courses.append(course)
+
+    return courses
