01c18e6bc7ea1202fec6febcce6edd263e1e8c75
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-04-15 02:56:06 -0400

    Update allocation.py

--- App/views/allocation.py
+++ App/views/allocation.py
@@ -1,23 +1,27 @@
-from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify, url_for
+from flask import flash, Blueprint, redirect, render_template, request, send_from_directory, jsonify, url_for
 from App.models import db
-from App.controllers import set_role, get_allocations, allocate_staff, get_course, get_available_staff, get_all_courses
+from App.controllers import is_allocated, set_role, get_allocations, allocate_staff, get_course, get_available_staff, get_all_courses
 
 allocation_views = Blueprint('allocation_views', __name__, template_folder='../templates')
 
 @allocation_views.route('/<course_id>', methods=['GET','POST'])
 def return_staff(course_id):
-    allocations = get_allocations()
+    allocations = get_allocations(course_id)
     courses = get_all_courses()
     staff = get_available_staff(course_id)
     return render_template('index.html', courseID=course_id, courses = courses, staff = staff,  allocations= allocations )
 
-@allocation_views.route('/<course_id>/<staff_id>', methods = ['GET','POST'])
-def allocateStaff(course_id, staff_id):
-    role =set_role(staff_id)
-    allocate = allocate_staff(course_id, staff_id, role)
-    allocations = get_allocations()
-    staff = get_available_staff(course_id)
+@allocation_views.route('/<course_id>/<staff_id>/<role>/<fname>/<lname>', methods = ['GET','POST'])
+def allocateStaff(course_id, staff_id, role, fname, lname):
+    id = str(course_id) + str(staff_id)
+    allocated = is_allocated(id)
     courses = get_all_courses()
-    return redirect ('/')
+    staff = get_available_staff(course_id)
+    allocations = get_allocations(course_id)
+    if allocated:
+        flash ('User already Allocated!')
+        return redirect (url_for('allocation_views.return_staff', course_id=course_id))
 
-    
+    allocate = allocate_staff(id, course_id, staff_id, role, fname, lname)
+    allocations = get_allocations(course_id)
+    return redirect (url_for('allocation_views.return_staff', course_id=course_id))
