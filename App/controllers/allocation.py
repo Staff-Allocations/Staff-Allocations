4943b21399d5309a861c0c9dc4e76bffec49dece
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-04-15 02:47:59 -0400

    Update allocation.py

--- App/controllers/allocation.py
+++ App/controllers/allocation.py
@@ -4,19 +4,21 @@ from App.config import config
 import requests
 import json
 
-def allocate_staff(course_id, staff_id, type):
-    if ((Allocation.query.filter_by(staffId = staff_id).first == None) and (Allocation.query.filter_by(courseId = course_id).first == None)):
-        newAllo = Allocation (staffId=staff_id, courseId=course_id, type = type)  
-        db.session.add (newAllo)
-        db.session.commit()
-        return newAllo
+def allocate_staff(id, course_id, staff_id, type, firstName, lastName):
+    newAllo = Allocation (id=id, staffId=staff_id, courseId=course_id, type=type, firstName=firstName, lastName=lastName)  
+    db.session.add (newAllo)
+    db.session.commit()
+    return newAllo
     return None
 
-def get_allocations():
-    return Allocation.query.all()
+def get_allocations(course_id):
+    return Allocation.query.filter_by(courseId=course_id)
+
+def is_allocated(Id):
+    allocated= Allocation.query.filter_by(id=Id).first()
+    return allocated
 
 def set_role(staff_id):
-    user = TeachingStaff.query.filter_by(id = staff_id).first
+    user = TeachingStaff.query.filter_by(id = staff_id).first()
     if (user):
         return user
-    
