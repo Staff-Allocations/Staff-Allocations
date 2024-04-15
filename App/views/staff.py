4c3352dc1d928e1fe1fd4a28eaba3e055352ad5d
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-04-15 02:56:42 -0400

    Update staff.py

--- App/views/staff.py
+++ App/views/staff.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
-
-from App.controllers import (get_all_ts, create_ts)
+import string
+from App.controllers import (get_all_ts, create_ts, update_staff, get_ts)
 
 staff_views = Blueprint('staff_views', __name__, template_folder='../templates')
 
@@ -14,14 +14,19 @@ def add_course():
     data = request.form
     staff_id = data['staffID']
     email = data['email']      
-    email = email.lower()   #some preprocessing 
     firstName = data['firstName']
     lastName = data['lastName']
     courses = data['courses']
-    courses = courses.replace(" ", "") #preprocessing
-    courses = courses.upper()
     type = data['type']
     status = data['status']
+
+    firstName = firstName.lower()   #preprocessing
+    firstName = string.capwords(firstName)
+    lastName = lastName.lower()
+    lastName = string.capwords(lastName)
+    courses = courses.replace(" ", "") 
+    courses = courses.upper()
+    email = email.lower()
 
     user = create_ts(staff_id, firstName, lastName, courses, email, type, status)
     if user:
@@ -34,6 +39,28 @@ def view_staff():
     staff = get_all_ts()
     return render_template('viewStaff.html', staff = staff)
 
-# @staff_views.route('/selectStaff', methods=['GET','POST'])
-# def select_staff():
-#     return render_template('selectType.html')
+@staff_views.route('/update_staff/<staff_id>', methods=['GET', 'POST'])
+def update_staff_view(staff_id):
+    staff = get_ts(staff_id)
+
+    if request.method == 'POST':
+        firstName = request.form['firstName']
+        lastName = request.form['lastName']
+        courses = request.form['courses']
+        email = request.form['email']
+        type = request.form['type']
+        status = request.form['status']
+        
+        firstName = firstName.lower()   #preprocessing
+        firstName = string.capwords(firstName)
+        lastName = lastName.lower()
+        lastName = string.capwords(lastName)
+        courses = courses.replace(" ", "") 
+        courses = courses.upper()
+        email = email.lower()
+
+        staff = update_staff(staff_id, firstName, lastName, courses, email, type, status)
+
+        return redirect(url_for('staff_views.view_staff'))
+
+    return render_template('updateStaff.html', staff=staff)
