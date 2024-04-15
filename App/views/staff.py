33304c3967fe79342dcded1a8ee4f0aa5a663f53
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-04-15 07:57:13 -0400

    Update staff.py

--- App/views/staff.py
+++ App/views/staff.py
@@ -1,22 +1,23 @@
 from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
 import string
-from App.controllers import (get_all_ts, create_ts, update_staff, get_ts)
+from App.controllers import (get_courses_for_staff, get_all_courses, delete_staff, get_all_ts, create_ts, update_staff, get_ts)
 
 staff_views = Blueprint('staff_views', __name__, template_folder='../templates')
 
 
 @staff_views.route('/addStaffDetails', methods=['GET','POST'])
 def add_course_page():
-    return render_template('addStaff.html')
+    courses = get_all_courses()
+    return render_template('addStaff.html', courses=courses)
 
 @staff_views.route('/addStaff', methods=['GET','POST'])
-def add_course():
+def add_staff():
     data = request.form
     staff_id = data['staffID']
     email = data['email']      
     firstName = data['firstName']
     lastName = data['lastName']
-    courses = data['courses']
+    courses = request.form.getlist('courses')
     type = data['type']
     status = data['status']
 
@@ -24,8 +25,6 @@ def add_course():
     firstName = string.capwords(firstName)
     lastName = lastName.lower()
     lastName = string.capwords(lastName)
-    courses = courses.replace(" ", "") 
-    courses = courses.upper()
     email = email.lower()
 
     user = create_ts(staff_id, firstName, lastName, courses, email, type, status)
@@ -37,16 +36,20 @@ def add_course():
 @staff_views.route('/viewStaff', methods=['GET','POST'])
 def view_staff():
     staff = get_all_ts()
+    # allocations = get_courses_for_staff()
     return render_template('viewStaff.html', staff = staff)
 
 @staff_views.route('/update_staff/<staff_id>', methods=['GET', 'POST'])
 def update_staff_view(staff_id):
     staff = get_ts(staff_id)
-
+    courses=get_all_courses()
     if request.method == 'POST':
         firstName = request.form['firstName']
         lastName = request.form['lastName']
-        courses = request.form['courses']
+        if 'courses' in request.form:
+            courses = request.form.getlist('courses')
+        else:
+            courses = []
         email = request.form['email']
         type = request.form['type']
         status = request.form['status']
@@ -55,12 +58,21 @@ def update_staff_view(staff_id):
         firstName = string.capwords(firstName)
         lastName = lastName.lower()
         lastName = string.capwords(lastName)
-        courses = courses.replace(" ", "") 
-        courses = courses.upper()
         email = email.lower()
 
         staff = update_staff(staff_id, firstName, lastName, courses, email, type, status)
 
         return redirect(url_for('staff_views.view_staff'))
 
-    return render_template('updateStaff.html', staff=staff)
+    return render_template('updateStaff.html', staff=staff, courses=courses)
+
+@staff_views.route('/delete_staff_routes/<staff_id>', methods=['POST'])
+def delete_staff_route(staff_id):
+    deleted = delete_staff(staff_id)
+    if deleted:
+        flash('Successfully deleted ', staff_id)
+        return redirect(url_for('staff_views.view_staff'))
+    else: 
+        flash('Error in deleting course')
+        return redirect(url_for('staff_views.view_staff'))
+      
