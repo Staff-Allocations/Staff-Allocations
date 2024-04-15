e09b0562e74dc5d4a902775061d24db5611ffffa
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-04-15 07:52:06 -0400

    Update courses.py

--- App/models/courses.py
+++ App/models/courses.py
@@ -1,30 +1,28 @@
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 from App.database import db
+
+
+course_staff_association = Table('course_staff_association',
+                                 db.Model.metadata,
+                                 Column('course_id', db.String(120), ForeignKey('courses.course_id')),
+                                 Column('user_id', Integer, ForeignKey('users.id'))
+                                )
 
 class Courses(db.Model):
     course_id = db.Column(db.String(120), primary_key=True)
     course_name = db.Column(db.String(120), nullable=False, unique=True)
     sem_offered = db.Column(db.Integer)
     type = db.Column(db.String(120)) 
-    staffAssigned = db.Column(db.Integer, db.ForeignKey ('users.id')) #Who's currently assigned to the course
-    currStudents = db.Column(db.Integer, nullable = True)
-    capacity = db.Column(db.Integer, nullable = True)
-    numAssessments = db.Column(db.Integer, nullable = False)
-    #totalCost = db.Column(db.Integer, nullable = True)
+    
+    staffAssigned = relationship("Users", secondary=course_staff_association)
+
+    currStudents = db.Column(db.Integer, nullable=True)
+    capacity = db.Column(db.Integer, nullable=True)
+    numAssessments = db.Column(db.Integer, nullable=False) 
     numStreams = db.Column(db.Integer)
+    totalCost = db.Column(db.Integer, nullable = True)
 
-    # def getStudents(): 
-
-    # def getStaff():
-
-    # def addStaff(): 
-
-    # def createCourse(): 
-
-    # def getNumAssessments(): 
-
-    # def numMarkersRequired(): 
-
-    # def getTotalCost():
 
     
 class Session(db.Model):
