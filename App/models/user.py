bc43f960a12432e76bcf794b2eaa186e702223da
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-04-16 04:31:15 -0400

    Update user.py

--- App/models/user.py
+++ App/models/user.py
@@ -5,10 +5,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username =  db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, email):
         self.username = username
         self.set_password(password)
+        self.email = email
 
     def get_json(self):
         return{
