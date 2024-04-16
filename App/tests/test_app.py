5a52e015eb92459f19fa4f713f51c07d7652d462
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-04-16 09:36:05 -0400

    Update test_app.py

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -22,12 +22,12 @@ LOGGER = logging.getLogger(__name__)
 class UserUnitTests(unittest.TestCase):
 
     def test_new_user(self):
-        user = User("bob", "bobpass")
+        user = User("bob", "bobpass, bob@mail")
         assert user.username == "bob"
 
     # pure function no side effects or integrations called
     def test_get_json(self):
-        user = User("bob", "bobpass")
+        user = User("bob", "bobpass, bob@mail")
         user_json = user.get_json()
         self.assertDictEqual(user_json, {"id":None, "username":"bob"})
     
@@ -57,13 +57,13 @@ def empty_db():
 
 
 def test_authenticate():
-    user = create_user("bob", "bobpass")
+    user = create_user("bob", "bobpass, bob@mail")
     assert login("bob", "bobpass") != None
 
 class UsersIntegrationTests(unittest.TestCase):
 
     def test_create_user(self):
-        user = create_user("rick", "bobpass")
+        user = create_user("rick", "bobpass, bob@mail")
         assert user.username == "rick"
 
     def test_get_all_users_json(self):
