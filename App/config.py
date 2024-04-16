ea2f1bfdd2b3dad4581431df08212abc843ef463
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2024-04-16 09:38:07 -0400

    Update config.py

--- App/config.py
+++ App/config.py
@@ -1,14 +1,18 @@
 import os
 import importlib
 
+# SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
+# SECRET_KEY = "secret key"
+# JWT_ACCESS_TOKEN_EXPIRES = 7
+# ENV = "DEVELOPMENT"
 
 # must be updated to inlude addtional secrets/ api keys & use a gitignored custom-config file instad
 def load_config():
     config = {'ENV': os.environ.get('ENV', 'DEVELOPMENT')}
     if config['ENV'] == "DEVELOPMENT":
         from .default_config import  SQLALCHEMY_DATABASE_URI, SECRET_KEY, JWT_SECRET_KEY
-        config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
-        config['SECRET_KEY'] = SECRET_KEY
+        config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///temp-database.db"
+        config['SECRET_KEY'] = "secret key"
     else:
         config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
         config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
